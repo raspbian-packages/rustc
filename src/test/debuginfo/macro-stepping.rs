88a28d25a108ff2aa73d12032032904afd6b3394
Rust Maintainers <pkg-rust-maintainers@lists.alioth.debian.org>
2017-01-04 20:34:56 +0000

    u-ignoretest-arm64
    ===================================================================
    
    Gbp-Pq: Name u-ignoretest-arm64.patch

--- src/test/debuginfo/macro-stepping.rs
+++ src/test/debuginfo/macro-stepping.rs
@@ -8,6 +8,7 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+// ignore-aarch64
 // ignore-windows
 // ignore-android
 // min-lldb-version: 310
