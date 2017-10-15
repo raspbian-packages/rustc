20dbe675704d861426b64625f93fed79c571808a
Rust Maintainers <pkg-rust-maintainers@lists.alioth.debian.org>
2017-10-15 22:30:35 +0100

    u-reproducible-build
    Gbp-Pq: Name u-reproducible-build.patch

--- src/librustc_llvm/build.rs
+++ src/librustc_llvm/build.rs
@@ -138,6 +138,11 @@ fn main() {
     let cxxflags = output(&mut cmd);
     let mut cfg = gcc::Config::new();
     for flag in cxxflags.split_whitespace() {
+        // Split-dwarf gives unreproducible DW_AT_GNU_dwo_id so don't do it
+        if flag == "-gsplit-dwarf" {
+            continue;
+        }
+
         // Ignore flags like `-m64` when we're doing a cross build
         if is_crossed && flag.starts_with("-m") {
             continue;
