db5b83dae223e86a6c585013cb9edaa851535204
Rust Maintainers <pkg-rust-maintainers@lists.alioth.debian.org>
2017-10-15 20:31:03 +0100

    d-dont-download-stage0
    Gbp-Pq: Name d-dont-download-stage0.patch

--- src/bootstrap/bootstrap.py
+++ src/bootstrap/bootstrap.py
@@ -24,15 +24,17 @@ import tempfile
 from time import time
 
 
-def get(url, path, verbose=False):
+def get(url, path, verbose=False, use_local_hash_if_present=True):
     sha_url = url + ".sha256"
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         temp_path = temp_file.name
-    with tempfile.NamedTemporaryFile(suffix=".sha256", delete=False) as sha_file:
-        sha_path = sha_file.name
+    sha_path = path + ".sha256"
 
     try:
-        download(sha_path, sha_url, False, verbose)
+        if use_local_hash_if_present and os.path.exists(sha_path):
+            print("using already-download file " + sha_path)
+        else:
+            download(sha_path, sha_url, False, verbose)
         if os.path.exists(path):
             if verify(path, sha_path, False):
                 if verbose:
@@ -50,7 +52,6 @@ def get(url, path, verbose=False):
             print("moving {} to {}".format(temp_path, path))
         shutil.move(temp_path, path)
     finally:
-        delete_if_present(sha_path, verbose)
         delete_if_present(temp_path, verbose)
 
 
@@ -180,7 +181,7 @@ class RustBuild(object):
                 rustc_channel, self.build)
             url = self._download_url + "/dist/" + self.stage0_date()
             tarball = os.path.join(rustc_cache, filename)
-            if not os.path.exists(tarball):
+            if True:
                 get("{}/{}".format(url, filename),
                     tarball, verbose=self.verbose)
             unpack(tarball, self.bin_root(),
@@ -190,7 +191,7 @@ class RustBuild(object):
             filename = "rustc-{}-{}.tar.gz".format(rustc_channel, self.build)
             url = self._download_url + "/dist/" + self.stage0_date()
             tarball = os.path.join(rustc_cache, filename)
-            if not os.path.exists(tarball):
+            if True:
                 get("{}/{}".format(url, filename),
                     tarball, verbose=self.verbose)
             unpack(tarball, self.bin_root(),
@@ -217,7 +218,7 @@ class RustBuild(object):
             filename = "cargo-{}-{}.tar.gz".format(cargo_channel, self.build)
             url = self._download_url + "/dist/" + self.stage0_date()
             tarball = os.path.join(rustc_cache, filename)
-            if not os.path.exists(tarball):
+            if True:
                 get("{}/{}".format(url, filename),
                     tarball, verbose=self.verbose)
             unpack(tarball, self.bin_root(),
