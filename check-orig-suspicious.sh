19032a5b4537a17d39b90433fb07dd02231296c5
Ximin Luo <infinity0@debian.org>
2017-10-25 19:27:30 +0100

    Import rustc_1.21.0+dfsg1-2.debian.tar.xz
    [dgit import tarball rustc 1.21.0+dfsg1-2 rustc_1.21.0+dfsg1-2.debian.tar.xz]

---
+++ check-orig-suspicious.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -x
+
+ver="$1"
+test -n "$ver" || exit 2
+
+FILTER="Files-Excluded: in debian/copyright and run a repack."
+SUS_WHITELIST=$(find "${PWD}" -name upstream-tarball-unsuspicious.txt -type f)
+
+rm -rf rustc-$ver-src/
+tar xf ../rustc_$ver+dfsg1.orig.tar.xz && cd rustc-$ver-src/
+
+# Remove non-suspicious files, warning on patterns that match nothing
+grep -v '^#' ${SUS_WHITELIST} | xargs  -I% sh -c 'rm -r ./% || true'
+echo "Checking for suspicious files..."
+
+# TODO: merge the -m stuff into suspicious-source(1).
+suspicious-source -v -m text/x-objective-c
+# The following shell snippet is a bit more strict than suspicious-source(1)
+find . -type f -and -not -name '.cargo-checksum.json' -exec file '{}' \; | \
+  sed -e 's/\btext\b\(.*\), with very long lines/verylongtext\1/g' | \
+  grep -v '\b\(text\|empty\)\b' || true
+
+# Most C and JS code should be in their own package
+find src/vendor/ -name '*.c' -o -name '*.js'
+
+echo "The above files (if any) seem suspicious, please audit them."
+echo "If good, add them to ${SUS_WHITELIST}."
+echo "If bad, add them to ${FILTER}."
+
+echo "Artifacts left in rustc-$ver-src, please remove them yourself."
