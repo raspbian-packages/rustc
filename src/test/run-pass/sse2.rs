4e4bd5d5f3ce096d373bb590dd7d8e641d6b29a3
Josh Stone <jistone@redhat.com>
2018-01-30 10:18:54 -0800

    Ignore run-pass/sse2 when using system LLVM
    This is a test of `target_feature`, which needs a rust-specific patch to
    LLVM to add `MCSubtargetInfo::getFeatureTable()`.
    
    Gbp-Pq: Name u-0001-Ignore-run-pass-sse2-when-using-system-LLVM.patch

--- src/test/run-pass/sse2.rs
+++ src/test/run-pass/sse2.rs
@@ -7,7 +7,7 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
-// min-llvm-version 4.0
+// no-system-llvm -- needs MCSubtargetInfo::getFeatureTable()
 
 #![feature(cfg_target_feature)]
 
