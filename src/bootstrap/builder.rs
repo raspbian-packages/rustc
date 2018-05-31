5e115098db4d32de42dd110d4c77d714101bba5e
Rust Maintainers <pkg-rust-maintainers@lists.alioth.debian.org>
2018-05-31 17:25:17 +0100

    d-ignore-removed-submodules
    Gbp-Pq: Name d-ignore-removed-submodules.patch

--- src/bootstrap/builder.rs
+++ src/bootstrap/builder.rs
@@ -803,10 +803,7 @@ impl<'a> Builder<'a> {
             }
         }
 
-        if self.config.locked_deps {
-            cargo.arg("--locked");
-        }
-        if self.config.vendor || self.is_sudo {
+        if self.is_sudo {
             cargo.arg("--frozen");
         }
 
