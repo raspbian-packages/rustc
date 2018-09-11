accd8a46cfb0b147a434c1e2c30caa22ca1dec34
Rust Maintainers <pkg-rust-maintainers@lists.alioth.debian.org>
2018-09-11 10:08:03 +0100

    u-powerpcspe-support
    Gbp-Pq: Name u-powerpcspe-support.patch

--- src/tools/build-manifest/src/main.rs
+++ src/tools/build-manifest/src/main.rs
@@ -82,6 +82,7 @@ static TARGETS: &'static [&'static str] = &[
     "mipsel-unknown-linux-gnu",
     "mipsel-unknown-linux-musl",
     "powerpc-unknown-linux-gnu",
+    "powerpc-unknown-linux-gnuspe",
     "powerpc64-unknown-linux-gnu",
     "powerpc64le-unknown-linux-gnu",
     "s390x-unknown-linux-gnu",
