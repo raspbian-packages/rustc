ff75a7e0259c67948a2a8fe08340d0a26914ee8f
Ximin Luo <infinity0@debian.org>
2018-11-02 08:42:36 +0000

    Import rustc_1.30.0+dfsg1-2.debian.tar.xz
    [dgit import tarball rustc 1.30.0+dfsg1-2 rustc_1.30.0+dfsg1-2.debian.tar.xz]

---
+++ check-orig-suspicious.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+ver="$1"
+test -n "$ver" || exit 2
+
+FILTER="Files-Excluded: in debian/copyright and run a repack."
+SUS_WHITELIST=$(find "${PWD}" -name upstream-tarball-unsuspicious.txt -type f)
+
+rm -rf rustc-${ver/*~*/beta}-src/
+tar xf ../rustc_$ver+dfsg1.orig.tar.xz && cd rustc-${ver/*~*/beta}-src/
+
+# Remove tiny files 4 bytes or less
+find . -size -4c -delete
+# Remove non-suspicious files, warning on patterns that match nothing
+echo "Excluding (i.e. removing) whitelisted files..."
+grep -v '^#' ${SUS_WHITELIST} | xargs  -I% sh -c 'rm -r ./% || true'
+echo "Checking for suspicious files..."
+
+# TODO: merge the -m stuff into suspicious-source(1).
+suspicious-source -v -m text/x-objective-c -m text/x-awk
+# The following shell snippet is a bit more strict than suspicious-source(1)
+find . -type f -and -not -name '.cargo-checksum.json' -exec file '{}' \; | \
+  sed -e 's/\btext\b\(.*\), with very long lines/verylongtext\1/g' | \
+  grep -v '\b\(text\|empty\)\b' || true
+
+# Most C and JS code should be in their own package
+find src/vendor/ -name '*.c' -o -name '*.js'
+
+echo "The above files (if any) seem suspicious, please audit them."
+echo "If good, add them to ${SUS_WHITELIST}."
+echo "If bad, add them to ${FILTER}."
+
+echo "Artifacts left in rustc-$ver-src, please remove them yourself."
