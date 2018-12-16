d2361fc240c7b2f9fb8be6c7676360bfe370eaec
Debian Rust Maintainers <pkg-rust-maintainers@alioth-lists.debian.net>
2018-12-16 22:34:44 +0000

    d-sparc64-dont-pack-spans
    Gbp-Pq: Name d-sparc64-dont-pack-spans.patch

--- src/libsyntax_pos/span_encoding.rs
+++ src/libsyntax_pos/span_encoding.rs
@@ -26,7 +26,9 @@ use std::hash::{Hash, Hasher};
 /// The primary goal of `Span` is to be as small as possible and fit into other structures
 /// (that's why it uses `packed` as well). Decoding speed is the second priority.
 /// See `SpanData` for the info on span fields in decoded representation.
-#[repr(packed)]
+
+/// Workaround for compiler bug: https://github.com/rust-lang/rust/issues/53181
+#[cfg_attr(not(any(target_arch = "sparc64", target_arch = "sparcv9")), repr(packed))]
 pub struct Span(u32);
 
 impl Copy for Span {}
