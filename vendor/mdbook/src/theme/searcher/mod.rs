774788ace55e27d15fadaeed4b54db36bcf77b19
Debian Rust Maintainers <pkg-rust-maintainers@alioth-lists.debian.net>
2019-01-28 06:02:48 +0000

    d-no-web-dependencies-in-doc
    Gbp-Pq: Name d-no-web-dependencies-in-doc.patch

--- vendor/mdbook/src/theme/searcher/mod.rs
+++ vendor/mdbook/src/theme/searcher/mod.rs
@@ -2,5 +2,3 @@
 //! the "search" cargo feature is disabled.
 
 pub static JS: &'static [u8] = include_bytes!("searcher.js");
-pub static MARK_JS: &'static [u8] = include_bytes!("mark.min.js");
-pub static ELASTICLUNR_JS: &'static [u8] = include_bytes!("elasticlunr.min.js");
