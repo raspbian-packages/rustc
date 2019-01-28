3846c54a5906d33e962a6895eeec3834fcfd394d
Debian Rust Maintainers <pkg-rust-maintainers@alioth-lists.debian.net>
2019-01-28 06:02:48 +0000

    u-prefer-local-css
    Gbp-Pq: Name u-prefer-local-css.patch

--- src/bootstrap/doc.rs
+++ src/bootstrap/doc.rs
@@ -419,7 +419,7 @@ impl Step for Standalone {
 
             if filename == "not_found.md" {
                 cmd.arg("--markdown-css")
-                   .arg("https://doc.rust-lang.org/rust.css");
+                   .arg("rust.css");
             } else {
                 cmd.arg("--markdown-css").arg("rust.css");
             }
