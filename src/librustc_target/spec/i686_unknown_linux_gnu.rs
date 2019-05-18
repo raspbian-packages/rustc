a3bb9e06b62faa92121367d92e3fd6412b0941ef
Debian Rust Maintainers <pkg-rust-maintainers@alioth-lists.debian.net>
2019-05-18 20:18:25 +0100

    d-i686-baseline
    Gbp-Pq: Name d-i686-baseline.patch

--- src/librustc_target/spec/i686_unknown_linux_gnu.rs
+++ src/librustc_target/spec/i686_unknown_linux_gnu.rs
@@ -2,7 +2,7 @@ use spec::{LinkerFlavor, Target, TargetResult};
 
 pub fn target() -> TargetResult {
     let mut base = super::linux_base::opts();
-    base.cpu = "pentium4".to_string();
+    base.cpu = "pentiumpro".to_string();
     base.max_atomic_width = Some(64);
     base.pre_link_args.get_mut(&LinkerFlavor::Gcc).unwrap().push("-m32".to_string());
     base.stack_probes = true;
